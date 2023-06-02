c3eedb9b26d8006c69c16175b4aa86a46d91fac5
Hedgehog <2973236988@qq.com>
2023-06-02 14:58:59 +0800

    Delete model_conversion.sh

--- model_conversion.sh
+++
@@ -1,6 +0,0 @@
-atc --model=./human-pose-estimation.onnx --framework=5 --output=openpose_pytorch_560 --soc_version=Ascend310 --input_shape="data:1, 3, 560, 560" --input_format=NCHW --insert_op_conf=./insert_op.cfg
-
-# 删除除 om 模型外额外生成的文件
-
-rm fusion_result.json
-rm -rf kernel_meta 
